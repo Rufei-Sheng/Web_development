ed3446d8ab2b467b9f1b0e614874ed26b744a8b2
Rufei <rufeisheng@gmail.com>
2019-04-09 22:01:21 -0400

    class practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,46 @@
 alert('hi there');
+
+var name = 'fei'  // identify a variable
+// var age = 22
+
+console.log(name) //log into console variable
+$('#alertButton').on('click', function() {
+	alert('Chris Whong');
+});
+
+$('#red-button').on('click', function() {
+  $('body').css('background', 'red');
+});
+
+$('#green-button').on('click', function() {
+  $('body').css('background', 'green');
+});
+
+$('#blue-button').on('click', function() {
+  $('body').css('background', 'blue');
+});
+
+$('.joes-pizza').on('click', function() {
+  $('#pizza-shop-info').text('Joes Pizza is the best in NYC');
+});
+
+$('.dominos-pizza').on('click', function() {
+  $('#pizza-shop-info').text('Dominos is delicious');
+});
+
+$('.papa-johns').on('click', function() {
+  $('#pizza-shop-info').text('Papa Johns is mediocre');
+});
+
+$('#ageSubmit').on('click', function(){
+  var age = $('#ageInput').val();
+  age = parseInt(age); //parse the number from string
+  console.log('Read the age from:'. age, age.length)
+})
+
+var arrayOfString = ['chirs', 'maggie', 'chrital']
+
+console.log(arrayOfString[0])
+
+// objects:like the dictionary on python
+var object = {}
